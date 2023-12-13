0b53c924d63b4cf569eb18dd73ec5144aa24288f
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-12-13 13:55:27 +0100

    More complex reporting pipeline

--- src/social_summarizer/pipelines/reporting/nodes.py
+++ src/social_summarizer/pipelines/reporting/nodes.py
@@ -9,16 +9,37 @@ import polars as pl
 from transformers.pipelines.base import Pipeline as HFPipeline
 
 
+def get_hashtags(df: pl.DataFrame, mask_filler, max_number_statuses: int) -> list[str]:
+    posts = df["text"].head(max_number_statuses).to_list()
+    results = mask_filler([post + " #<mask>" for post in posts])
+
+    hashtags = set()
+    for ht_list in results:
+        for hashtag in ht_list:
+            hashtags.add(hashtag["token_str"])
+
+    return list(hashtags)
+
+
 def write_executive_summary(
-    statuses: pl.DataFrame, model: HFPipeline, max_num_statuses: int
+    statuses: pl.DataFrame, summarizer: HFPipeline, max_num_statuses: int
 ) -> str:
     statuses_text = statuses.head(max_num_statuses)["text"].to_list()
     short_prompt = """
-    This is a list of posts that Luis Villa posted this week on Mastodon:
+    This is a list of posts:
 
     {}
-    """.format("\n\n".join([f"{post}" for post in statuses_text]))
-
-    executive_summary_results = model(short_prompt)
+    """.format("\n\n".join(post for post in statuses_text))
+    executive_summary_results = summarizer(short_prompt)
 
     return fill(executive_summary_results[0]["summary_text"])
+
+
+def consolidate_report(hashtags, summary):
+    report = f"""List of topics: {hashtags[:5]}
+
+    Summary:
+
+    {summary}
+    """
+    return report
--- src/social_summarizer/pipelines/reporting/pipeline.py
+++ src/social_summarizer/pipelines/reporting/pipeline.py
@@ -5,12 +5,21 @@ generated using Kedro 0.18.14
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import write_executive_summary
+from .nodes import consolidate_report, get_hashtags, write_executive_summary
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
+            node(
+                func=get_hashtags,
+                inputs=[
+                    "statuses_clean",
+                    "fill_mask_model",
+                    "params:reporting.max_num_statuses",
+                ],
+                outputs="hashtags_list",
+            ),
             node(
                 func=write_executive_summary,
                 inputs=[
@@ -18,7 +27,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "cnn.summarizer_model",
                     "params:reporting.max_num_statuses",
                 ],
+                outputs="summary_text",
+            ),
+            node(
+                func=consolidate_report,
+                inputs=["hashtags_list", "summary_text"],
                 outputs="executive_summary",
-            )
+            ),
         ]
     )
